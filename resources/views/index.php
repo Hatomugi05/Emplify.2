793c4f4d72d178600192109da18fbffe8ea03b2b
Hatomugi05 <davidadlay@gmail.com>
2024-11-14 23:46:16 -0600

    first commit

---
+++ resources/views/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('public/css/style.css')">
+</head>
+<body>
+    <header class="header">
+        <div class="menu container">
+            <a href="#" class="logo">Emplify</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+                <img src="imagenes/menu.png" class="menu-icono" alt="Menu Icono">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Iniciar sesión</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="heade-content container">
+            <div class="header-txt">
+                <h1>Ahora es más fácil encontar <span>empleo</span> desde tu casa</h1>
+                <p>Con nuestra plataforma, podrás encontar el trabajo que mejor se te adecue</p>
+            </div>
+        </div>
+    </header>
+
+    <section class="about container">
+        <div class="about-img">
+            <img src="imagenes/b1.jpg" alt="">
+            <img src="imagenes/b2.jpg" alt="">
+        </div>
+        
+        <div class="about-txt">
+            <h2>Se parte de las personas con su  <span>sueño</span> de trabajar</h2>
+            <p>Con nuestra plataforma, podrás encontrararas un empleo</p>
+        </div>
+    </section>
+
+    <main class="products container">
+
+        <h2>Empleos <span>disponibles </span></h2>
+
+        <div class="products-content">
+            <div class="product">
+                <img src="imagenes/1.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/2.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/3.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/4.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/5.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/6.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/7.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/8.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+
+            <div class="product">
+                <img src="imagenes/9.jpg" alt="">
+                <div class="product-txt">
+                    <h3>aqui aparece el trabajo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Hic, dolorum recusan
+
+                    </p>
+                    <a href="#" class="btn-2">Informacion</a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <section class="infomation">
+        <div class="infomation-content container">
+            <div class="information-1"></div>
+            <div class="information-2">
+                <h2>De donde surgio <span>Emplify</span></h2>
+                <p>Emplify surgio para apoyar a las pequeñas empresas a publicar sus vacantes</p>
+                <div class="img-content">
+                    <img src=" " alt="">
+                    <img src=" " alt="">
+                    <img src=" " alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    </div>
+    <section class="contact container">
+        <h2>Contacto</h2>
+        <form>
+            <input type="email" placeholder="Correo">
+            <input type="submit" value="enviar" class="btn-3">
+        </form>
+
+    </section>
+
+    <footer class="footer">
+
+        <div class="footer-content container">
+
+            <div class="link">
+                <h3>lorem</h3>
+                    <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+
+            <div class="link">
+                <h3>lorem</h3>
+                    <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+
+            <div class="link">
+                <h3>lorem</h3>
+                    <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+
+            <div class="link">
+                <h3>lorem</h3>
+                    <ul>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                    <li><a href="#">Lorem</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+</html>
